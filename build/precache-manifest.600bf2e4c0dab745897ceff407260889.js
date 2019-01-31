4da8baab322249f208e427fb040a271a89d2cca2
onofrio <onofrio@yopmail.com>
2019-01-31 18:23:22 +0100

    initial commit

---
+++ build/precache-manifest.600bf2e4c0dab745897ceff407260889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89b89b14d559d4bfc1ae",
+    "url": "/static/js/main.89b89b14.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "89b89b14d559d4bfc1ae",
+    "url": "/static/css/main.1b24f298.chunk.css"
+  },
+  {
+    "revision": "6bec8cf852f53104bacb05c6ca0e0f68",
+    "url": "/index.html"
+  }
+];
